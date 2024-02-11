86adc54f5198e2b0d37d07be8f9bd89434aec7f7
Gökay Şendil <gokaysendilw@outlook.com>
2024-02-11 14:35:19 +0300

    Sign In and Sign Up page completed(Frontend)
    To do:
    Implement firebase email&password login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import { AuthContextProvider } from './context/AuthContext'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Account from './pages/Account'
 
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path ='/' element={<Home/>}/>
+      <Route path ='/login' element={<Login/>}/>
+      <Route path ='/signup' element={<SignUp/>}/>
+      <Route path ='/account' element={<Account/>}/>
     </Routes>
     </AuthContextProvider>
     </>
