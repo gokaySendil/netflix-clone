2f3acd096e9d0cb86b4453675305e0daef471411
Gökay Şendil <gokaysendilw@outlook.com>
2024-02-11 12:00:17 +0300

    Row Component completed
    Now we can display 5 rows with different categories. Works on mobile screens too.
    ToDo : Setup Firebase

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import MainComponent from '../components/MainComponent'
+import Row from '../components/Row'
+import requests from '../Requests'
 
 const Home = () => {
   return (
-    <div>
-        <Navbar/>
+    <>
+        
         <MainComponent/>
-    </div>
+        <Row rowID = '1' title ='Upcoming' fetchURL ={requests.requestUpcoming}/>
+        <Row rowID = '2' title ='Popular' fetchURL ={requests.requestPopular}/>
+        <Row rowID = '3' title ='Trending' fetchURL ={requests.requestTrending}/>
+        <Row rowID = '4' title ='Top Rated' fetchURL ={requests.requestTopRated}/>
+        <Row rowID = '5' title ='Horror' fetchURL ={requests.requestHorror}/>
+
+    </>
   )
 }
 
