21f70737cbe971835944a1f806ed7fd4519a053c
Gökay Şendil <gokaysendilw@outlook.com>
2024-02-11 13:57:04 +0300

    Firebase Connected
    .env file errors fixed
    To Do : Sign In&Up pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import { AuthContextProvider } from './context/AuthContext'
 
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
     <Navbar/>
     <Routes>
       <Route path ='/' element={<Home/>}/>
     </Routes>
+    </AuthContextProvider>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig ,transformWithEsbuild  } from 'vite'
+import envCompatible from 'vite-plugin-env-compatible'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  envPrefix:"REACT_APP_",
+  plugins: [
+    envCompatible(),
+    {
+      name: 'treat-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/))  return null
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        })
+      },
+    },
+    react(),
+  ],
+  optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
