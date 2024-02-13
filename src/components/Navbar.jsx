21118ff9a4c43ac4258994b54fb1d605e8d4f703
Gökay Şendil <gokaysendilw@outlook.com>
2024-02-14 00:55:24 +0300

    Firestore data saving completed
    Working on the account page
    Todo
    List the liked movies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
             <button className="text-white pr-4">Sign In</button>
           </Link>
           <Link to={`/signup`}>
-            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer">
+            <button className="bg-red-600 px-6 py-2 text-white rounded cursor-pointer">
               Sign Up
             </button>
           </Link>
